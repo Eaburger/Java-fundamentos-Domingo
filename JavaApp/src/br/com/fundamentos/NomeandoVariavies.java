07b2ada4ac201d1c4b76697fa0661778eaf19a47
Eduardo Augusto Burger <32910@PROWAY.TREINA>
2020-09-05 17:45:38 -0300

    4 - Nomeando Variavies

---
+++ JavaApp/src/br/com/fundamentos/NomeandoVariavies.java
@@ -0,0 +1,31 @@
+package br.com.fundamentos;
+
+public class NomeandoVariavies {
+	public static void main(String[] args) {
+		//Valido separado por undeline	
+		String variavel_separada_por_undeline;
+		
+		//Padrao camelcase
+		
+		String variavelSeparadaComUnderline;
+		
+		// Valido Porem Estranho
+		
+		int _numero=1;
+		
+		System.out.println(_numero);
+		
+		//Valido Porem Mais Estranho ainda parece PHP
+		
+		int $outroNumero= 1 ;
+		
+		System.out.println($outroNumero);
+		
+		String 1um;
+				
+				
+		
+		
+	}
+
+}
