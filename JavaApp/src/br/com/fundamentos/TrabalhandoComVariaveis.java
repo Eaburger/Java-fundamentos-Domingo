f5631e86b41266f2899ca796cda101e7f1e9f438
Eduardo Augusto Burger <32910@PROWAY.TREINA>
2020-09-05 16:34:18 -0300

    2 - Palavras reservadas

---
+++ JavaApp/src/br/com/fundamentos/TrabalhandoComVariaveis.java
@@ -0,0 +1,21 @@
+package br.com.fundamentos;
+
+public class TrabalhandoComVariaveis {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	int quantidade;//Declarando variaveis
+	quantidade=10;// atribuindo valor 10
+	
+	System.out.println(quantidade);
+	
+	quantidade =15;
+	
+	System.out.println(quantidade);
+	
+	}
+	
+
+}
