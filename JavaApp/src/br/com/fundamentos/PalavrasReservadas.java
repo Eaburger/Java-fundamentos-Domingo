a3494e282b99be11656a4f1a703ad26a95f7c0d7
Eduardo Augusto Burger <32910@PROWAY.TREINA>
2020-09-05 16:19:53 -0300

    1 - Aula caracteres Escape

---
+++ JavaApp/src/br/com/fundamentos/PalavrasReservadas.java
@@ -0,0 +1,14 @@
+package br.com.fundamentos;
+
+public class PalavrasReservadas {
+/*
+ * Palavras Resavadas sao palavras que a linguegem utiliza
+ * como exemplo:int,string,double
+ *  
+ * http://www.linhadecodigo.com.br/artigo/83/as-52-palavras-reservadas-do-java.aspx
+ * 
+ */
+	
+	int numero;
+	
+}
