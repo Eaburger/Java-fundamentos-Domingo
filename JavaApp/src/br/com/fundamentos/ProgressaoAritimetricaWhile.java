6493f8c1431177636c8705f7cbddb4d736e453e6
Eduardo Augusto Burger <32910@PROWAY.TREINA>
2020-09-20 17:14:39 -0300

    20/09

---
+++ JavaApp/src/br/com/fundamentos/ProgressaoAritimetricaWhile.java
@@ -0,0 +1,23 @@
+package br.com.fundamentos;
+
+public class ProgressaoAritimetricaWhile {
+	public static void main(String[] args) {
+		
+		int inicial=1;
+		int razao=3;
+		int an= inicial;
+		int valor_max = 20;
+		
+		System.out.println("Elementos da Pa de valor incial %d e razao %d, menores que %d\n", inicial,razao,valor_max);
+		
+		while (an<= valor_max) {
+			
+			System.out.println(an);
+			an += razao;
+		}
+		
+		
+	}
+	
+
+}
