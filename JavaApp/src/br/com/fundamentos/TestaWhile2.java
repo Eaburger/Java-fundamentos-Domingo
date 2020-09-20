ed8f11f8bb078833251cac05858a20475b5db97c
Eduardo Augusto Burger <32910@PROWAY.TREINA>
2020-09-20 16:24:19 -0300

    20/09

---
+++ JavaApp/src/br/com/fundamentos/TestaWhile2.java
@@ -0,0 +1,23 @@
+package br.com.fundamentos;
+
+public class TestaWhile2 {
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		
+		while (contador <=10) {
+			
+			int total = 0;
+			total = total + contador;
+			
+			System.out.println(total);
+			
+			contador ++ ;
+			
+			
+			
+		}
+		
+	}
+
+}
